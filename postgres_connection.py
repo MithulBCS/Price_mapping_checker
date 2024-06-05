24c50dfeaa7bbe072a0fd828509aa75e8fbf9ea0
Vserve-User <mithul.murugaadev@building-controls.com>
2024-06-05 19:17:10 +0530

    added v2

--- postgres_connection.py
+++ postgres_connection.py
@@ -1,4 +1,13 @@
 import psycopg2
+import logging
+from datetime import datetime
+import os
+
+current_time = datetime.now()
+fcurrent_time = current_time.strftime("%Y-%m-%d-%H-%M-%S")
+log_file = os.path.join("D:\\Automation\\Logging_information\\", f"Pricing_automation_{fcurrent_time}.log")
+logging.basicConfig(filename=log_file, level=logging.DEBUG)
+
 
 # Connect to your PostgreSQL database
 def connect_to_postgres(dbname, user, password, host, port):
@@ -10,12 +19,32 @@ def connect_to_postgres(dbname, user, password, host, port):
             host=host,
             port=port
         )
-        print("Connected to the PostgreSQL database successfully!")
+        logging.info("Connected to the PostgreSQL database successfully!")
         return conn
+    
     except psycopg2.Error as e:
-        print("Unable to connect to the database")
-        print(e)
+        logging.error("Unable to connect to the database")
+        logging.error(e)
         return None
+
+
+def read_data_into_table(connection, company_df):
+
+    # filter only the discrepancy ones
+    df = company_df[company_df["Matched_pricingdoc_SPN"] == "Not available"]
+    
+    cursor = connection.cursor()
+
+    for index, row in df.iterrows():
+        stspn = row["Stripped_supplier_PN"]
+        # add the other columns that are needed to be a part of the SQL database as well 
+
+
+
+        query = "Insert into <table> values >>>"
+
+        cursor.execute(query)
+
 
 
 def export_table_to_csv(connection, table_name, output_file):
@@ -23,24 +52,11 @@ def export_table_to_csv(connection, table_name, output_file):
         cursor = connection.cursor()
         with open(output_file, 'w') as f:
             cursor.copy_expert(f"COPY {table_name} TO STDOUT WITH CSV HEADER", f)
-        print(f"Data from table '{table_name}' successfully exported to '{output_file}'")
+        logging.info(f"Data from table '{table_name}' successfully exported to '{output_file}'")
         
     except psycopg2.Error as e:
-        print(f"Error exporting data from table '{table_name}' to CSV file")
-        print(e)
+        logging.error(f"Error exporting data from table '{table_name}' to CSV file")
+        logging.error(e)
 
 
-# Example usage
-if __name__ == "__main__":
-    dbname = 'your_db_name'
-    user = 'your_username'
-    password = 'your_password'
-    host = 'your_host'  # 'localhost' if it's your local machine
-    port = 'your_port_number'  # Default PostgreSQL port is 5432
-    connection = connect_to_postgres(dbname, user, password, host, port)
-    if connection:
-        # Perform database operations here
-        export_table_to_csv(connection, table_name, output_file)
-        connection.close()  # Close the connection when done
 
-# give a copy command to save the data in csv files
